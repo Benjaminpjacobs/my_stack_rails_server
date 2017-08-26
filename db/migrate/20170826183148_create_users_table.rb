43a374c089f7a1bc224904f47cd387c64bbf37ba
Benjamin Jacobs <benjaminpjacobs@gmail.com>
2017-08-26 12:32:28 -0600

    creates users table

---
+++ db/migrate/20170826183148_create_users_table.rb
@@ -0,0 +1,12 @@
+class CreateUsersTable < ActiveRecord::Migration[5.1]
+  def change
+    create_table :users do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :email
+      t.string :provider
+      t.string :token
+      t.string :uid
+    end
+  end
+end
