2c9fc0d1a599ac4f70bf2103c1877816af57d7c7
Benjamin Jacobs <benjaminpjacobs@gmail.com>
2017-08-27 09:53:15 -0600

    lists all repos and creates hooks for each

--- app/controllers/hooks/hook_controller.rb
+++ app/controllers/hooks/hook_controller.rb
@@ -1,23 +1,33 @@
 class Hooks::HookController < ApplicationController
   def create
-    username = User.first.username
-    token = User.first.token
     conn = Faraday.new(:url => "https://api.github.com") 
-    resp = conn.post do |req|
-      req.url "/repos/#{current_user.username}/2DoBox-Pivot/hooks"
+    repos = conn.get do |req|
+      req.url "/user/repos?type"
       req.headers['Authorization'] = "token #{current_user.token}"
-      req.body = '{
-        "name": "web",
-        "active": true,
-        "events": [
-          "push",
-          "pull_request"
-        ],
-        "config": {
-          "url": "https://4024f579.ngrok.io/hooks/github",
-          "content_type": "json"
-        }
-        }'
+      req.params['type'] = 'all'
+      req.params['sort'] = 'updated'
+      req.params['per_page'] = '100'
+    end
+    
+    repo_list = JSON.parse(repos.body).map{|repo| repo['full_name']}
+    
+    repo_list.each do |repo|
+      conn.post do |req|
+        req.url "/repos/#{repo}/hooks"
+        req.headers['Authorization'] = "token #{current_user.token}"
+        req.body = '{
+          "name": "web",
+          "active": true,
+          "events": [
+            "push",
+            "pull_request"
+          ],
+          "config": {
+            "url": "https://f41cdce5.ngrok.io/hooks/github",
+            "content_type": "json"
+          }
+          }'
+        end
       end
     end
 end
