a4fc85847a9a54f03c40d9cb0560133be50c8e9d
Benjamin Jacobs <benjaminpjacobs@gmail.com>
2017-09-12 16:04:58 -0600

    put reception code back in

--- app/controllers/hooks/google/reception_controller.rb
+++ app/controllers/hooks/google/reception_controller.rb
@@ -6,28 +6,28 @@ class Hooks::Google::ReceptionController < HookBaseController
 
     user = User.find_by_email(parsed['emailAddress'])
     id = user.identities.where(provider: 'google_oauth2').first
-    # token = Token.new(id)
-    # token.access_token
-    # client = Signet::OAuth2::Client.new(access_token: token.access_token)
-    # client.expires_in = Time.now + 1_000_000
-    # service = Google::Apis::GmailV1::GmailService.new
-    # service.authorization = client
+    token = Token.new(id)
+    token.access_token
+    client = Signet::OAuth2::Client.new(access_token: token.access_token)
+    client.expires_in = Time.now + 1_000_000
+    service = Google::Apis::GmailV1::GmailService.new
+    service.authorization = client
     
-    # messages = service.list_user_messages('me')
-    # id =messages.messages.first.id
-    # msg = service.get_user_message('me', id)
+    messages = service.list_user_messages('me')
+    id =messages.messages.first.id
+    msg = service.get_user_message('me', id)
 
-    # subject =  msg.payload.headers.select{|header| header.name == "Subject"}.first.value
-    # from =  msg.payload.headers.select{|header| header.name == "From"}.first.value
-    # snippet = msg.snippet
-    # data = msg.payload.parts.first.body.data
-    # payload = {from: from, snippet: snippet, data: data, subject: subject}
+    subject =  msg.payload.headers.select{|header| header.name == "Subject"}.first.value
+    from =  msg.payload.headers.select{|header| header.name == "From"}.first.value
+    snippet = msg.snippet
+    data = msg.payload.parts.first.body.data
+    payload = {from: from, snippet: snippet, data: data, subject: subject}
     
-    # if msg.label_ids.include?('UNREAD') && msg.label_ids.include?('INBOX')
-    #   Message.store(payload, user, 'message', 'google_oauth2')
-    #   service = WebsocketService.new
-    #   service.post_message({user_id: user.id, service_id: 3})
-    # end
+    if msg.label_ids.include?('UNREAD') && msg.label_ids.include?('INBOX')
+      Message.store(payload, user, 'message', 'google_oauth2')
+      service = WebsocketService.new
+      service.post_message({user_id: user.id, service_id: 3})
+    end
   end
 
 end
