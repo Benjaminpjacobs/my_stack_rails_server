2410fbcb25fb021d4c44e2cc6aaf23a16b02528a
Benjamin Jacobs <benjaminpjacobs@gmail.com>
2017-09-17 15:21:59 -0600

    all hook settings tested

--- app/controllers/hooks/google/broadcast_controller.rb
+++ app/controllers/hooks/google/broadcast_controller.rb
@@ -1,15 +1,23 @@
 class Hooks::Google::BroadcastController < ApplicationController
+  before_action :set_identity
+  before_action :set_google_service
     def new
-      id = current_user.identities.where(provider: 'google_oauth2').first
-      google_service = GoogleService.new({id: id})
-      google_service.update_service
+      @google_service.update_service
       redirect_to request.referer
     end
 
     def delete
-      id = current_user.identities.where(provider: 'google_oauth2').first
-      google_service = GoogleService.new({id: id})
-      google_service.cancel_service
+      @google_service.cancel_service
       redirect_to request.referer
+    end
+
+    private
+    
+    def set_identity
+      @id = current_user.identities.where(provider: 'google_oauth2').first
+    end
+
+    def set_google_service
+      @google_service = GoogleService.new({id: @id})
     end
 end
--- spec/features/user_can_set_hooks_spec.rb
+++ spec/features/user_can_set_hooks_spec.rb
@@ -2,22 +2,107 @@ require 'rails_helper'
 
 RSpec.describe "Login" do
   include Devise::Test::IntegrationHelpers
-  context "as a user" do
-    it "can login with github" do
-      
-      github   = create(:service, name: 'github')
-      id    = create(:identity, provider: github.name, uid: 'U70Q8ND1U')
-      user  = id.user
-      binding.pry
-      
-      sign_in user
+  describe "as a user" do
+    context "it sees correct options"do
+      it "when logged in with github" do
+        
+        github   = create(:service, name: 'github')
+        id    = create(:identity, provider: github.name, uid: 'U70Q8ND1U')
+        user  = id.user
+        
+        sign_in user
 
-      visit hooks_path
-      expect(page).to have_content('Set Github Hooks')
-      expect(page).to have_content('Authorize Google')
-      expect(page).to have_content('Authorize Slack')
+        visit hooks_path
 
-      
+        expect(page).to have_content('Set Github Hooks')
+        expect(page).to have_content('Authorize Google')
+        expect(page).to have_content('Authorize Slack')
+      end
+      it "when logged in with google" do
+        
+        google   = create(:service, name: 'google_oauth2')
+        id    = create(:identity, provider: google.name, uid: 'U70Q8ND1U')
+        user  = id.user
+        
+        sign_in user
+
+        visit hooks_path
+
+        expect(page).to have_content('Set Google Hooks')
+        expect(page).to have_content('Authorize Github')
+        expect(page).to have_content('Authorize Slack')
+      end
+      it "when logged in with slack" do
+        
+        slack   = create(:service, name: 'slack')
+        id    = create(:identity, provider: slack.name, uid: 'U70Q8ND1U')
+        user  = id.user
+        
+        sign_in user
+
+        visit hooks_path
+
+        expect(page).to have_content('Delete Slack Hooks')
+        expect(page).to have_content('Authorize Google')
+        expect(page).to have_content('Authorize Github')
+      end
+    end
+    
+    context "it can set and delete hooks" do
+      it "for github" do 
+        allow_any_instance_of(GithubService).to receive(:set_all_web_hooks).and_return(true)
+        allow_any_instance_of(GithubService).to receive(:delete_all_web_hooks).and_return(true)
+
+        github   = create(:service, name: 'github')
+        id    = create(:identity, provider: github.name, uid: 'U70Q8ND1U')
+        user  = id.user
+        
+        sign_in user
+
+        visit hooks_path
+        click_link 'Set Github Hooks'
+        expect(current_path).to eq(hooks_path)
+        expect(page).to have_content('Delete Github Hooks')
+        expect(page).to have_content('Update Github Hooks')
+
+        click_link 'Delete Github Hooks'
+        expect(current_path).to eq(hooks_path)
+        expect(page).to have_content('Set Github Hooks')
+      end
+
+      it "for google" do 
+        google   = create(:service, name: 'google_oauth2')
+        id    = create(:identity, provider: google.name, uid: 'U70Q8ND1U')
+        user  = id.user
+
+        allow_any_instance_of(Google::Apis::GmailV1::GmailService).to receive(:watch_user).and_return(true)
+        allow_any_instance_of(Google::Apis::GmailV1::GmailService).to receive(:stop_user).and_return(true)
+        
+        sign_in user
+
+        visit hooks_path
+        click_link 'Set Google Hooks'
+        expect(current_path).to eq(hooks_path)
+        expect(page).to have_content('Delete Google Hooks')
+
+        click_link 'Delete Google Hooks'
+        expect(current_path).to eq(hooks_path)
+        expect(page).to have_content('Set Google Hooks')
+      end
+
+      it "for slack" do 
+        slack   = create(:service, name: 'slack')
+        id    = create(:identity, provider: slack.name, uid: 'U70Q8ND1U')
+        user  = id.user
+
+        sign_in user
+
+        visit hooks_path
+        click_link 'Delete Slack Hooks'
+        expect(current_path).to eq(hooks_path)
+        expect(page).to have_content('Authorize Slack')
+        expect(user.identities.empty?).to be true
+      end
     end
   end
 end
